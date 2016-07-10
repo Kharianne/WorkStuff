dc3bcb25b15bcc8213db943e501deb321f176169
Kharianne <katerinamrkackova@gmail.com>
2016-07-10 14:24:53 +0200

    Polishing and adding new functions Added url parser - domain adn cs/sk Added readHTML to avoid duplicate code Added itemPrice to check price of first item on page

--- Functions.py
+++ Functions.py
@@ -1,10 +1,41 @@
 import argparse
+import re
+import urllib.request
+from bs4 import BeautifulSoup
 
+
+
+#Reads url from command line
 def urlParser():
     parser = argparse.ArgumentParser(description="Fill in url")
     parser.add_argument('--url', action = "store", type = str)
     args = parser.parse_args()
     values = vars(args)
     url = (values['url'])
-    return url
+    if url is None:
+        print ("URL argument missing")
+        exit()
+    else:
+        return url
 
+#Parse url into domain and cz/sk
+def whereAmI(url):
+    try:
+        domainLanguage = re.findall('^https?://(?:www)\.(.+)\.(.+)', url)
+        return domainLanguage
+    except:
+        print ("URL neni v pozadovanem formatu!")
+        exit()
+
+#Returns BS4 object to work with
+def readHTML(url):
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+#Finds first product on a page and returns its price
+def itemPrice(class_, tag, soup):
+    product= soup.find(class_=class_)
+    priceTag = product.find(tag, {"itemprop" : "price"}).attrs
+    price = float(priceTag['content'])
+    return price
--- PriceCheck.py
+++ PriceCheck.py
@@ -1,11 +1,29 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import Functions
 
-html = urllib.request.urlopen("https://www.bigbrands.cz/vsechny-produkty?order=price-asc").read()
-soup = BeautifulSoup(html)
-product= soup.find(class_="product-box__info__price")
-priceTag = product.find("span", {"itemprop" : "price"}).attrs
-price = float(priceTag['content'])
-if price < 1:
-	print('There is some product below 1 CZK')
-print(price)
+url = Functions.urlParser()
+#url = "https://www.bambino.sk"
+domainLanguage = Functions.whereAmI(url)
+
+if domainLanguage[0][1] == "cz":
+    add = "/vsechny-produkty?order=price-asc"
+    url = url + add
+
+else:
+    add = "/vsetky-produkty?order=price-asc"
+    url = url + add
+
+soup = Functions.readHTML(url)
+domain = domainLanguage[0][0]
+if domain == "bigbrands":
+    price = Functions.itemPrice("product-box__info__price","span",soup)
+    print(price)
+elif domain == "prodeti" or domain == "bambino":
+    price = Functions.itemPrice("product-box__info--right", "strong",soup)
+    print(price)
+elif domain == "kolonial":
+     price = Functions.itemPrice("product-box__info__price","strong",soup)
+     print(price)
+else:
+    print("Domain is not supported.")
